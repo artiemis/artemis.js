135da4b876a85683b497cb419a2e646997a61937
artie <artiehime@gmail.com>
2025-02-20 23:11:11 +0100

    fnm fix

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,5 +1,8 @@
 ssh lia "
 set -e
+export PATH=\"\$HOME/.local/share/pnpm:\$PATH\"
+eval \"\`\$HOME/.local/share/fnm/fnm env --shell=bash\`\"
+
 cd artemis.js
 git pull
 ~/.bun/bin/bun install --frozen-lockfile
