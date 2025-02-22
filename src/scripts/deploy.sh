ef9db12cb8ca2bac878433c661edd71f05c2a447
artie <artiehime@gmail.com>
2025-02-22 17:52:37 +0100

    docker deploy

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,10 +1,6 @@
 ssh lia "
 set -e
-export PATH=\"\$HOME/.local/share/pnpm:\$PATH\"
-eval \"\`\$HOME/.local/share/fnm/fnm env --shell=bash\`\"
-
 cd artemis.js
 git pull
-~/.bun/bin/bun install --frozen-lockfile
-pm2 restart artemis
+docker compose up -d --build
 "
